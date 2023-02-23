a5116f67cc4dc6949208c9f49f94ebe2a902e5e4
demo <demo@zuehlke.com>
2023-02-23 17:18:08 +0000

    commit

--- demo_process_01/run.py
+++ demo_process_01/run.py
@@ -1,1 +1,13 @@
 import pandas
+
+# %sql memory
+
+# result_df = %sql select * from '~/sample_data/credit_history.parquet' limit 100;
+
+result_df.head()
+
+result_df['my_new_column'] = result_df['mortgage_due'] + result_df['credit_card_due']
+
+result_df.head()
+
+result_df.to_parquet('~/data/my_semi_table1.parquet')
